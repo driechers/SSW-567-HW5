ab08f75891dd6315273d66c47b523b95445eee0e
Daniel Riechers <driechers101@gmail.com>
2019-09-26 20:22:01 -0500

    Pylinted code
    Added pylintrc file and made changes so that pylint passes
    
    Signed-off-by: Daniel Riechers <driechers101@gmail.com>

--- classify.py
+++ classify.py
@@ -1,37 +1,52 @@
 #!/usr/bin/python3
+"""
+The purpose of this module is to classify a triangle as isoceles, scalene, equalateral or right
+"""
 
 import sys
 import math
 
+
 def classify_triangle(a, b, c):
+    """
+    The purpose of this function is to classify a triangle as
+    isoceles, scalene, equalateral or right
+
+    The order of operations do not matter
+    Right takes priority over scalene and isocelese
+
+    """
     # Make c the largest value
-    x = max([a,b,c])
-    if x == a:
-        a,c = c,a
-    if x == b:
-        b,c = c,b
+    max_side = max([a, b, c])
+    if max_side == a:
+        a, c = c, a
+    if max_side == b:
+        b, c = c, b
 
     # Determine validity
     if a + b <= c:
-        return 'invalid'
-    if math.isinf(a) or math.isinf(b) or math.isinf(c):
-        return 'invalid'
-    if math.isnan(a) or math.isnan(b) or math.isnan(c):
-        return 'invalid'
+        classification = 'invalid'
+    elif math.isinf(a) or math.isinf(b) or math.isinf(c):
+        classification = 'invalid'
+    elif math.isnan(a) or math.isnan(b) or math.isnan(c):
+        classification = 'invalid'
 
     # Determine if right
-    if a**2 + b**2 == c**2:
-        return 'right'
+    elif a**2 + b**2 == c**2:
+        classification = 'right'
 
     # Determine if eqalateral
-    if a == b and b == c:
-        return 'equilateral'
+    elif a == b and b == c:
+        classification = 'equilateral'
 
     # Determine if isosceles
-    if a == b or b == c or a == c:
-        return 'isosceles'
+    elif a == b or b == c or a == c:
+        classification = 'isosceles'
 
-    return 'scalene'
+    else:
+        classification = 'scalene'
+
+    return classification
 
 if __name__ == '__main__':
     print(classify_triangle(
